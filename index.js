7c195b5ff63f8531811c3780d7c8c8bb4beca1e5
merine <merinemutitu@gmail>
2021-12-26 09:21:45 +0100

    Passanger Counter app

---
+++ index.js
@@ -0,0 +1,29 @@
+//document.getElementById("count-el").innerText = 9;
+
+
+
+
+
+// initialize the count 
+// listen for the clicks on the increment button
+// increment the count variable when the button is clicked 
+// change the counter in the HTML to reflect the new count 
+
+
+
+let count = 0;
+function increase (){
+   count = count + 1
+   console.log(document.getElementById("count-el").innerText = count);
+
+}
+
+
+
+//create a save button that logs out the count when its called
+function save(){
+   console.log(count)
+    
+}
+
+
